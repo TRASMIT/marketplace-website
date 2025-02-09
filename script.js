10efbe97a0508feebeb801b852d8b5097a52366d
TRASMIT <vasukiran864@gmail.com>
2025-02-09 14:25:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const books = [
+    { title: "Book 1", pdf: "book1.pdf" },
+    { title: "Book 2", pdf: "book2.pdf" },
+    { title: "Book 3", pdf: "book3.pdf" }
+];
+const bookGrid = document.getElementById("bookGrid");
+function displayBooks() {
+    bookGrid.innerHTML = "";
+    books.forEach(book => {
+        const bookElement = document.createElement("div");
+        bookElement.classList.add("book");
+        bookElement.innerHTML = `<h3>${book.title}</h3><a href="${book.pdf}" target="_blank">Read Online</a>`;
+        bookGrid.appendChild(bookElement);
+    });
+}
+displayBooks();
