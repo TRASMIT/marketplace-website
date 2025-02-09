1335525e7ecfef1ae1e36a19e0ef267e5c6b2576
TRASMIT <vasukiran864@gmail.com>
2025-02-09 15:35:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const books = [
     { title: "Dracula by Bram Stoker (1897 Edition)", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/DRACULA%20by%20Bram%20Stoker.pdf" },
     { title: "Pride and Prejudice", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/pride-and-prejudice.pdf" },
     { title: "Little Women", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/little-women.pdf" },
-    { title: "Frankenstein", pdf: "https://github.com/TRASMIT/marketplace-website/blob/main/E%20Books/frankenstein.pdf" }
+    { title: "Frankenstein", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/frankenstein.pdf" }
 ];
 
 const bookGrid = document.getElementById("bookGrid");
