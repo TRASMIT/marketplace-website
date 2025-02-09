0bf45a0a5ec07f6f37d7fd88ee1de25b1d789eaa
TRASMIT <vasukiran864@gmail.com>
2025-02-09 18:06:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const books = [
-    { title: "The Great Gatsby", cover: "https://github.com/TRASMIT/marketplace-website/blob/main/Book%20Covers/The%20Great%20Gatsby%20Book%20Cover.jpg", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/the-great-gatsby.pdf" },
+    { title: "The Great Gatsby", cover: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/Book%20Covers/The%20Great%20Gatsby%20Book%20Cover.jpg", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/the-great-gatsby.pdf" },
     { title: "The Odyssey by Homer", cover: "https://github.com/TRASMIT/marketplace-website/blob/main/Book%20Covers/61hheEthD8L._SL1360_.jpg", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/The_Odyssey.pdf" },
     { title: "Dracula by Bram Stoker (1897 Edition)", cover: "https://github.com/TRASMIT/marketplace-website/blob/main/Book%20Covers/61jl29eQPSL._UF1000%2C1000_QL80_.jpg", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/DRACULA%20by%20Bram%20Stoker.pdf" },
     { title: "Pride and Prejudice", cover: "https://github.com/TRASMIT/marketplace-website/blob/main/Book%20Covers/il_fullxfull.1519409460_3v5o.avif", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/pride-and-prejudice.pdf" },
