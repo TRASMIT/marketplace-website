78170001630fc48f34b140994ded7c6bb6f08a8d
TRASMIT <vasukiran864@gmail.com>
2025-02-09 15:26:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const books = [
-    { title: "The Great Gatsby", pdf: "https://github.com/TRASMIT/marketplace-website/blob/main/E%20Books/the-great-gatsby.pdf" },
-    { title: "The Odessy by homer", pdf: "https://github.com/TRASMIT/marketplace-website/blob/main/E%20Books/The_Odyssey.pdf" },
-    { title: "Dracula by Bram Stoker 1897 edition", pdf: "https://github.com/TRASMIT/marketplace-website/blob/main/E%20Books/DRACULA%20by%20Bram%20Stoker.pdf" },
-    { title: "Pride and Prejudice", pdf: "https://github.com/TRASMIT/marketplace-website/blob/main/E%20Books/pride-and-prejudice.pdf" },
-    { title: "Little Women", pdf: "https://github.com/TRASMIT/marketplace-website/blob/main/E%20Books/little-women.pdf" }
+    { title: "The Great Gatsby", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/the-great-gatsby.pdf" },
+    { title: "The Odyssey by Homer", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/The_Odyssey.pdf" },
+    { title: "Dracula by Bram Stoker (1897 Edition)", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/DRACULA%20by%20Bram%20Stoker.pdf" },
+    { title: "Pride and Prejudice", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/pride-and-prejudice.pdf" },
+    { title: "Little Women", pdf: "https://raw.githubusercontent.com/TRASMIT/marketplace-website/main/E%20Books/little-women.pdf" }
 ];
+
 const bookGrid = document.getElementById("bookGrid");
 function displayBooks() {
     bookGrid.innerHTML = "";
